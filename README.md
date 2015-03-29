0133aefe651c7e513fcfc470f802a79a6274d548
Xavier Via <fernando.via@gmail.com>
2015-03-29 15:27:30 +0200

    Update README

--- README.md
+++ README.md
@@ -3,17 +3,232 @@ object-pattern
 
 [![Codeship status](https://codeship.com/projects/168ec210-8ee6-0132-51af-0a0cf4fe8e66/status?branch=master)](https://codeship.com/projects/61105)
 
-Object Pattern structures for Node.js
+Object Pattern structures for JavaScript.
 
-Matchable
----------
+`object-pattern` provides a fast and complete analog of Regular Expressions
+but aimed to describe generic object structures instead of strings.
+
+What was in my mind while creating this? Simple: creating EventEmitters
+that support listening to object structures as the event descriptions.
+
+A prime example of when this would be useful are REST interfaces.
+`object-pattern` could be used as a drop-in replacement of regular
+endpoints in a [Express.js](http://expressjs.com/) or
+[Sinatra](http://www.sinatrarb.com/) like routing framework.
+
+Installation
+------------
+
+From NPM:
+
+```
+npm install --save object-pattern
+```
+
+From Bower:
+
+```
+bower install --save object-pattern
+```
+
+`object-pattern` supports CommonJS, AMD and globals, so feel free to require
+it as best fits you.
+
+Usage
+-----
+
+The most straightforward way of using `object-pattern` is with the notation
+language (Object Pattern Notation):
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse("property:'value'");
+
+pattern.match({ property: "value" }); // => true!
+```
+
+Exactly like Regular Expressions, Object Patterns match if the target object
+_contains_ at the very least the structure described. If you do:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse("property:'value'");
+
+pattern.match({
+  property: "value",
+  something: ["extra", { and: "complex" } ]
+}); // => true!
+```
+
+...it will still return `true` since the property `property` with value
+`"value"` exists in the object.
+
+You can also use wildcards for the value:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse("property:*");
+
+pattern.match({ property: 50 }); // => true
+pattern.match({ property: "else" }); // => true
+pattern.match({ property: false }); // => true
+pattern.match({ property: { structure: true } }); // => still true!
+```
+
+...and also wildcards for the property name:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse("*:value");
+
+pattern.match({ anything: "value" }); // => true
+```
+
+You can make sure that a property with some value doesn't exist:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('!property:value');
+
+pattern.match({ property: "value" }); // => false
+```
+
+You can also make sure that the value is of a
+[certain JSON type](https://github.com/xaviervia/object-pattern/issues/2#issuecomment-87404454):
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('property:<number>');
+
+pattern.match({ property: 20 }); // => true
+pattern.match({ property: -7.20 }); // => true
+pattern.match({ property: "-7.20" }); // => false
+```
+
+There are also array patterns, both standalone:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('/user/<number>');
+
+pattern.match(["user", 50]); // => true
+```
+
+...and within an object structure:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('method:GET,path:/articles/*');
+
+pattern.match({
+  method: "GET",
+  path: ["articles", "rest-is-awesome"]
+}); // =>  true
+```
+
+There are also ellipsis, to match any amount of items in an array.
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('/**/saturn');
+
+pattern.match(["planets", "saturn"]); // => true
+pattern.match(["planets", 6, "many-moons", "saturn"]); // => true
+pattern.match(["saturn"]); // => true
+```
+
+Ellipsis matchers are non-greedy.
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('/**/saturn/**/moons');
+
+pattern.match(["saturn", 62, "moons"]); // => true
+pattern.match(["saturn", 62, "moons", "saturn", 62, "moons"]); // => false
+```
+
+Object and array structures can be nested:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('userAgent:(os:"linux",browser:"firefox")');
+
+pattern.match({
+  userAgent: {
+    os: "linux",
+    browser: "firefox",
+    device: "tablet"
+  }
+}); // => true
+```
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('/[/users/*]/[/articles/*]');
+
+pattern.match([
+  ["users", 60],
+  ["articles", 70]
+]); // => true
+```
+
+Once you have the pattern structure, you can stringify it again into OPN:
+
+```javascript
+var OP = require("object-pattern");
+
+var pattern = OP.parse('some:structure,withTypes:<number>');
+
+pattern.toString(); // => "some:'structure',withTypes:<number>"
+```
+
+Keep reading for a complete reference of the `object-pattern` types (the
+structures into which the OPN string compiles while parsed) and how to
+interact with them low-level.
+
+### Advanced usage note
+
+Most of the `object-pattern` structure looks for descendants of `Matchable`
+or `ArrayMatchable` to delegate matching operations. You can create your
+own object matching structures by inheriting from those functions and
+assembling the pattern structure yourself. The only drawback is that it's
+not going to be available to be parsed from the OPN, but that could be OK
+in many cases (and you can always pull request if you think your
+extension should be standard).
+
+Pattern Structures
+------------------
+
+All of this structures are accessible from the main object. To create an
+`ObjectPattern` structure from scratch, you coud do, for example:
+
+```javascript
+var OP = require("object-pattern");
+
+new OP.ObjectPattern(
+  new OP.ExactProperty("property", "value")
+);
+```
+
+### Matchable
 
 A common parent for all matchables. The interface that they are supposed to
 implement (although `Matchable` itself does not) is to expose a `match`
 method that returns either `true` or `false`.
 
-WildcardProperty
-----------------
+### WildcardProperty
 
 Returns `true` if the value of any property is `===` to the assigned value.
 `false` otherwise. If initialized with an inheritor of `Matchable` it will
@@ -33,8 +248,7 @@ wildcardProperty = new WildcardProperty(matchable);
 wildcardProperty.match({"property": "value"}); // => true
 ```
 
-ExactProperty
--------------
+### ExactProperty
 
 Returns `true` if there is a property with the given name which value is
 `===` to the assigned value. `false` otherwise.
@@ -62,8 +276,7 @@ exactProperty = new ExactProperty("project", matchable);
 exactProperty.match({"property": "value"}); // => false
 ```
 
-Negator
--------
+### Negator
 
 Delegates the matching to the sent matchable and negates the result.
 
@@ -78,8 +291,7 @@ var negator = new Negator(matchable);
 negator.match({"here": "ignored"}); // => false
 ```
 
-ObjectPattern
--------------
+### ObjectPattern
 
 Returns the `&&` result of calling the `match` method in each `properties`,
 forwarding the argument.
@@ -99,8 +311,7 @@ property.match({
 }) // => true
 ```
 
-WildcardValue
--------------
+### WildcardValue
 
 Returns always `true` except if the argument is `undefined`.
 
@@ -110,8 +321,7 @@ var wildcardValue = new WildcardValue();
 wildcardValue.match("something"); // => true
 ```
 
-TypedValue
-----------
+### TypedValue
 
 If initialized with a `Function`, returns `true` only if the argument if
 `instanceof` the `Function`.
@@ -138,8 +348,7 @@ var typedValue = new TypedValue(Type);
 typedValue.match(new Type()) // => true
 ```
 
-ArrayPattern
-------------
+### ArrayPattern
 
 Handles `ArrayMatchable`s, combining their results to return a final
 `Boolean` value representing whether the `Array` was or not a match.
@@ -158,8 +367,7 @@ arrayMatcher.match([6, 'user', 9]); // => false
 arrayMatcher.match([-56.2, 'user', 'extra', 9]); // => true
 ```
 
-ArrayMatchable
---------------
+### ArrayMatchable
 
 A common parent for all descriptors of `Array` components. `ArrayMatchable`s
 have a slightly different interface than regular `Matchable`s because they
@@ -167,8 +375,7 @@ need to send back the chunk of the Array that wasn't consumed by the current
 pattern so that the `ArrayPattern` can forward it to the next
 `ArrayMatchable`.
 
-ArrayEllipsis
--------------
+### ArrayEllipsis
 
 The `ArrayEllipsis` represents a variable length pattern, and it's behavior
 depends on how it is configured.
@@ -201,13 +408,12 @@ result.matched; // => true
 result.unmatched; // => ['extra']
 ```
 
-parse
------
+### parse
 
 Parses an [OPN (Object Pattern Notation)](https://github.com/xaviervia/sydney/wiki/Object-Pattern-Notation)
 string and returns the corresponding pattern structure.
 
-### Usage
+Usage:
 
 ```javascript
 var parse = require("object-pattern").parse
@@ -220,3 +426,12 @@ pattern.match({
 ```
 
 For more examples please refer to the [OPN examples](OPN.js)
+
+License
+-------
+
+Copyright 2014 Xavier Via
+
+ISC license.
+
+See [LICENSE](LICENSE) attached.
--- object-pattern.js
+++ object-pattern.js
@@ -3,14 +3,224 @@
 //
 // [![Codeship status](https://codeship.com/projects/168ec210-8ee6-0132-51af-0a0cf4fe8e66/status?branch=master)](https://codeship.com/projects/61105)
 //
-// Object Pattern structures for Node.js
+// Object Pattern structures for JavaScript.
 //
-// Matchable
-// ---------
+// `object-pattern` provides a fast and complete analog of Regular Expressions
+// but aimed to describe generic object structures instead of strings.
 //
-// A common parent for all matchables. The interface that they are supposed to
-// implement (although `Matchable` itself does not) is to expose a `match`
-// method that returns either `true` or `false`.
+// What was in my mind while creating this? Simple: creating EventEmitters
+// that support listening to object structures as the event descriptions.
+//
+// A prime example of when this would be useful are REST interfaces.
+// `object-pattern` could be used as a drop-in replacement of regular
+// endpoints in a [Express.js](http://expressjs.com/) or
+// [Sinatra](http://www.sinatrarb.com/) like routing framework.
+//
+// Installation
+// ------------
+//
+// From NPM:
+//
+// ```
+// npm install --save object-pattern
+// ```
+//
+// From Bower:
+//
+// ```
+// bower install --save object-pattern
+// ```
+//
+// `object-pattern` supports CommonJS, AMD and globals, so feel free to require
+// it as best fits you.
+//
+// Usage
+// -----
+//
+// The most straightforward way of using `object-pattern` is with the notation
+// language (Object Pattern Notation):
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse("property:'value'");
+//
+// pattern.match({ property: "value" }); // => true!
+// ```
+//
+// Exactly like Regular Expressions, Object Patterns match if the target object
+// _contains_ at the very least the structure described. If you do:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse("property:'value'");
+//
+// pattern.match({
+//   property: "value",
+//   something: ["extra", { and: "complex" } ]
+// }); // => true!
+// ```
+//
+// ...it will still return `true` since the property `property` with value
+// `"value"` exists in the object.
+//
+// You can also use wildcards for the value:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse("property:*");
+//
+// pattern.match({ property: 50 }); // => true
+// pattern.match({ property: "else" }); // => true
+// pattern.match({ property: false }); // => true
+// pattern.match({ property: { structure: true } }); // => still true!
+// ```
+//
+// ...and also wildcards for the property name:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse("*:value");
+//
+// pattern.match({ anything: "value" }); // => true
+// ```
+//
+// You can make sure that a property with some value doesn't exist:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('!property:value');
+//
+// pattern.match({ property: "value" }); // => false
+// ```
+//
+// You can also make sure that the value is of a
+// [certain JSON type](https://github.com/xaviervia/object-pattern/issues/2#issuecomment-87404454):
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('property:<number>');
+//
+// pattern.match({ property: 20 }); // => true
+// pattern.match({ property: -7.20 }); // => true
+// pattern.match({ property: "-7.20" }); // => false
+// ```
+//
+// There are also array patterns, both standalone:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('/user/<number>');
+//
+// pattern.match(["user", 50]); // => true
+// ```
+//
+// ...and within an object structure:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('method:GET,path:/articles/*');
+//
+// pattern.match({
+//   method: "GET",
+//   path: ["articles", "rest-is-awesome"]
+// }); // =>  true
+// ```
+//
+// There are also ellipsis, to match any amount of items in an array.
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('/**/saturn');
+//
+// pattern.match(["planets", "saturn"]); // => true
+// pattern.match(["planets", 6, "many-moons", "saturn"]); // => true
+// pattern.match(["saturn"]); // => true
+// ```
+//
+// Ellipsis matchers are non-greedy.
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('/**/saturn/**/moons');
+//
+// pattern.match(["saturn", 62, "moons"]); // => true
+// pattern.match(["saturn", 62, "moons", "saturn", 62, "moons"]); // => false
+// ```
+//
+// Object and array structures can be nested:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('userAgent:(os:"linux",browser:"firefox")');
+//
+// pattern.match({
+//   userAgent: {
+//     os: "linux",
+//     browser: "firefox",
+//     device: "tablet"
+//   }
+// }); // => true
+// ```
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('/[/users/*]/[/articles/*]');
+//
+// pattern.match([
+//   ["users", 60],
+//   ["articles", 70]
+// ]); // => true
+// ```
+//
+// Once you have the pattern structure, you can stringify it again into OPN:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// var pattern = OP.parse('some:structure,withTypes:<number>');
+//
+// pattern.toString(); // => "some:'structure',withTypes:<number>"
+// ```
+//
+// Keep reading for a complete reference of the `object-pattern` types (the
+// structures into which the OPN string compiles while parsed) and how to
+// interact with them low-level.
+//
+// ### Advanced usage note
+//
+// Most of the `object-pattern` structure looks for descendants of `Matchable`
+// or `ArrayMatchable` to delegate matching operations. You can create your
+// own object matching structures by inheriting from those functions and
+// assembling the pattern structure yourself. The only drawback is that it's
+// not going to be available to be parsed from the OPN, but that could be OK
+// in many cases (and you can always pull request if you think your
+// extension should be standard).
+//
+// Pattern Structures
+// ------------------
+//
+// All of this structures are accessible from the main object. To create an
+// `ObjectPattern` structure from scratch, you coud do, for example:
+//
+// ```javascript
+// var OP = require("object-pattern");
+//
+// new OP.ObjectPattern(
+//   new OP.ExactProperty("property", "value")
+// );
+// ```
 //
 "use strict";
 
@@ -38,6 +248,12 @@
 
 })("OP", function () {
 
+  // ### Matchable
+  //
+  // A common parent for all matchables. The interface that they are supposed to
+  // implement (although `Matchable` itself does not) is to expose a `match`
+  // method that returns either `true` or `false`.
+  //
   var Matchable = function () {}
 
   var toString = function (value) {
@@ -53,8 +269,7 @@
     return "'" + value + "'"
   }
 
-  // WildcardProperty
-  // ----------------
+  // ### WildcardProperty
   //
   // Returns `true` if the value of any property is `===` to the assigned value.
   // `false` otherwise. If initialized with an inheritor of `Matchable` it will
@@ -84,7 +299,7 @@
 
   WildcardProperty.prototype.match = function (object) {
     var key = undefined
-    
+
     if (this.value instanceof Matchable) {
       for (key in object) if (this.value.match(object[key])) return true }
 
@@ -100,8 +315,7 @@
 
 
 
-  // ExactProperty
-  // -------------
+  // ### ExactProperty
   //
   // Returns `true` if there is a property with the given name which value is
   // `===` to the assigned value. `false` otherwise.
@@ -152,8 +366,7 @@
 
 
 
-  // Negator
-  // -------
+  // ### Negator
   //
   // Delegates the matching to the sent matchable and negates the result.
   //
@@ -184,8 +397,7 @@
 
 
 
-  // ObjectPattern
-  // -------------
+  // ### ObjectPattern
   //
   // Returns the `&&` result of calling the `match` method in each `properties`,
   // forwarding the argument.
@@ -230,8 +442,7 @@
 
 
 
-  // WildcardValue
-  // -------------
+  // ### WildcardValue
   //
   // Returns always `true` except if the argument is `undefined`.
   //
@@ -255,8 +466,7 @@
 
 
 
-  // TypedValue
-  // ----------
+  // ### TypedValue
   //
   // If initialized with a `Function`, returns `true` only if the argument if
   // `instanceof` the `Function`.
@@ -325,8 +535,7 @@
 
 
 
-  // ArrayPattern
-  // ------------
+  // ### ArrayPattern
   //
   // Handles `ArrayMatchable`s, combining their results to return a final
   // `Boolean` value representing whether the `Array` was or not a match.
@@ -407,8 +616,7 @@
 
 
 
-  // ArrayMatchable
-  // --------------
+  // ### ArrayMatchable
   //
   // A common parent for all descriptors of `Array` components. `ArrayMatchable`s
   // have a slightly different interface than regular `Matchable`s because they
@@ -420,8 +628,7 @@
 
 
 
-  // ArrayEllipsis
-  // -------------
+  // ### ArrayEllipsis
   //
   // The `ArrayEllipsis` represents a variable length pattern, and it's behavior
   // depends on how it is configured.
@@ -496,13 +703,12 @@
   }
 
 
-  // parse
-  // -----
+  // ### parse
   //
   // Parses an [OPN (Object Pattern Notation)](https://github.com/xaviervia/sydney/wiki/Object-Pattern-Notation)
   // string and returns the corresponding pattern structure.
   //
-  // ### Usage
+  // Usage:
   //
   // ```javascript
   // var parse = require("object-pattern").parse
@@ -692,3 +898,13 @@
     parse: parse
   }
 })
+
+//
+// License
+// -------
+//
+// Copyright 2014 Xavier Via
+//
+// ISC license.
+//
+// See [LICENSE](LICENSE) attached.
